a37bbe785adefdf13f1d680e4593e16c28384564
pitzzahh <pitzzahh@araopj.tech>
2023-06-29 16:34:29 +0800

    chore: add model

---
+++ MediaClippex/MVVM/Model/Video.cs
@@ -0,0 +1,9 @@
+﻿namespace MediaClippex.MVVM.Model;
+
+public class Video
+{
+    private string? ImageUrl { get; set; }
+    private string? Title { get; set; }
+    private string? Duration { get; set; }
+    private string? Description { get; set; }
+}
