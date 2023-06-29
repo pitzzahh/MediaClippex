3151b057c5de9474fe7375bd487a4371e0ef35bc
pitzzahh <pitzzahh@araopj.tech>
2023-06-29 16:35:15 +0800

    chore: add async service for getting videos

---
+++ MediaClippex/Services/VideoService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using VideoLibrary;
+
+namespace MediaClippex.Services;
+
+public static class VideoService
+{
+    public static async Task<Video> GetVideo(string url)
+    {
+        return await YouTube.Default.GetVideoAsync(url);
+    }
+    
+    public static async Task<IEnumerable<Video>> GetAllVideos(string url)
+    {
+        return await YouTube.Default.GetAllVideosAsync(url);
+    }
+    
+}
