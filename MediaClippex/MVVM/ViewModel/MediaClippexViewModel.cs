d772cf872c8feb9f5a5cbee74661f514e55fe9a7
pitzzahh <pitzzahh@araopj.tech>
2023-06-29 08:32:58 +0800

    chore: add default values

--- MediaClippex/MVVM/ViewModel/MediaClippexViewModel.cs
+++ MediaClippex/MVVM/ViewModel/MediaClippexViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Windows;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
@@ -23,21 +25,47 @@ public partial class MediaClippexViewModel : ObservableValidator
     [ObservableProperty]
     private string? _imagePreview;
 
-    [ObservableProperty] private bool _canDownload = false;
+    [ObservableProperty] private bool _isResolved;
     
     [ObservableProperty]
     private ObservableCollection<string> _formats = new();
+
+    [ObservableProperty]
+    private string? _selectedFormat;
     
     [ObservableProperty]
     private ObservableCollection<string> _qualities = new();
+    
+    [ObservableProperty]
+    private string? _selectedQuality;
 
+    public MediaClippexViewModel()
+    {
+        Formats.Add("mp4");
+        Formats.Add("mp3");
+        Formats.Add("wav");
+        Formats.Add("ogg");
+        Formats.Add("webm");
+        Formats.Add("flac");
+        Formats.Add("m4a");
+        SelectedFormat = Formats.First();
+    }
+
+    [RelayCommand]
+    private void Resolve()
+    {
+        if (string.IsNullOrWhiteSpace(Url))
+        {
+            MessageBox.Show("Please enter a URL.");
+            return;
+        }
+        IsResolved = true;
+    }
 
     [RelayCommand]
     private void Download()
     {
-        if (string.IsNullOrWhiteSpace(Url))
-        {
-            return;
-        }
+        
     }
+    
 }
