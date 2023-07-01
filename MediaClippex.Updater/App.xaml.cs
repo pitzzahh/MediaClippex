6be719196a716d7389cc8558664d0565c09b451f
pitzzahh <pitzzahh@araopj.tech>
2023-07-01 23:37:54 +0800

    chore: add

---
+++ MediaClippex.Updater/App.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+using Russkyc.DependencyInjection.Implementations;
+
+namespace MediaClippex.Updater;
+
+/// <summary>
+///     Interaction logic for App.xaml
+/// </summary>
+public partial class App
+{
+    private static Mutex? _mutex;
+    public App()
+    {
+        BuilderServices.BuildWithContainer(BuildContainer.ConfigureServices());
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        _mutex = new Mutex(true, "MediaClippexUpdaterMutex", out var createdNew);
+
+        if (!createdNew)
+        {
+            // An instance of the application is already running, bring it to foreground and exit.
+            NativeMethods.BringWindowToForeground(NativeMethods.FindWindow("Updater", "MediaClippex Updater"));
+            Current.Shutdown();
+            return;
+        }
+        BuilderServices.Resolve<MainWindow>().Show();
+        base.OnStartup(e);
+    }
+    
+    protected override void OnExit(ExitEventArgs e)
+    {
+        _mutex?.ReleaseMutex();
+        _mutex?.Dispose();
+        base.OnExit(e);
+    }
+    
+    internal static class NativeMethods
+    {
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        internal static extern bool BringWindowToForeground(IntPtr hWnd);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+        internal static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+    }
+}
---
+++ MediaClippex.Updater/BuildContainer.cs
@@ -0,0 +1,17 @@
+﻿using Russkyc.DependencyInjection.Implementations;
+using Russkyc.DependencyInjection.Interfaces;
+
+namespace MediaClippex.Updater;
+
+// Container builder
+public static class BuildContainer
+{
+    // Returns container with registered dependencies
+    public static IServicesContainer ConfigureServices()
+    {
+        return new ServicesCollection()
+            .AddSingleton<MediaClippexUpdaterViewModel>()
+            .AddTransient<MediaClippexUpdater>()
+            .Build();
+    }
+}
