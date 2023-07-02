216b211f9a646cf26a321fea146bfd5c6161784c
pitzzahh <pitzzahh@araopj.tech>
2023-07-02 12:31:36 +0800

    chore: add code behind for downloading and installing update

---
+++ MediaClippex.Updater/MVVM/View/MediaClippexUpdater.xaml.cs
@@ -0,0 +1,12 @@
+﻿using MediaClippex.Updater.MVVM.ViewModel;
+
+namespace MediaClippex.Updater.MVVM.View;
+
+public partial class MediaClippexUpdater
+{
+    public MediaClippexUpdater()
+    {
+        InitializeComponent();
+        DataContext = new MediaClippexUpdaterViewModel();
+    }
+}
