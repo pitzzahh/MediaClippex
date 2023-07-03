bd9ed445b50539cce1e58a726a4a57374927d0f4
pitzzahh <pitzzahh@araopj.tech>
2023-07-03 09:52:41 +0800

    chore: add file check

--- Launcher/App.xaml.cs
+++ Launcher/App.xaml.cs
@@ -1,8 +1,21 @@
-﻿namespace Launcher;
+﻿using System.Diagnostics;
+using System.IO;
+using System.Windows;
+
+namespace Launcher;
 
 /// <summary>
 /// Interaction logic for App.xaml
 /// </summary>
 public partial class App
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        if (File.Exists("ffmpeg.exe"))
+        {
+            Process.Start("MediaClippex.exe");
+            Current.Shutdown();
+        }
+        else base.OnStartup(e);
+    }
 }
