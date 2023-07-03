10330c1b3ff2a78dd6c3aa30a1819fe9a3821518
pitzzahh <pitzzahh@araopj.tech>
2023-07-03 08:36:10 +0800

    chore: fix references

--- Launcher/MVVM/ViewModel/LauncherViewModel.cs
+++ Launcher/MVVM/ViewModel/LauncherViewModel.cs
@@ -1,4 +1,8 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Diagnostics;
+using System.Net.Mime;
+using System.Windows;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Launcher.MVVM.ViewModel;
 
@@ -8,6 +12,14 @@ public partial class LauncherViewModel : ObservableObject
     [ObservableProperty] private string _progressBarVisibility = "Collapsed";
     [ObservableProperty] private string? _progressInfo;
     [ObservableProperty] private bool _isProgressIndeterminate;
-    
-    
+
+    [RelayCommand]
+    private void Download()
+    {
+        Process.Start("MediaClippex.exe");
+        Application.Current.Dispatcher.Invoke(() =>
+        {
+            Application.Current.Shutdown();
+        });
+    }
 }
