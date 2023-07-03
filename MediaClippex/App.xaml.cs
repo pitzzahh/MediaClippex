5d7b9f53edb50e16edc78997bc86d369e9f15299
pitzzahh <pitzzahh@araopj.tech>
2023-07-03 22:07:10 +0800

    chore: removed unused code

--- MediaClippex/App.xaml.cs
+++ MediaClippex/App.xaml.cs
@@ -1,8 +1,15 @@
-﻿namespace MediaClippex;
+﻿using MediaClippex.Services;
+using Russkyc.DependencyInjection.Implementations;
+
+namespace MediaClippex;
 
 /// <summary>
 ///     Interaction logic for App.xaml
 /// </summary>
 public partial class App
 {
+    public App()
+    {
+        BuilderServices.BuildWithContainer(BuildContainer.ConfigureServices());
+    }
 }
