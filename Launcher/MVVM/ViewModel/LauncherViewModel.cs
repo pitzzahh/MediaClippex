32dea3e226301f22dcaf5aed6da13d9e751354e0
pitzzahh <pitzzahh@araopj.tech>
2023-07-03 08:23:29 +0800

    chore: add observables

--- Launcher/MVVM/ViewModel/LauncherViewModel.cs
+++ Launcher/MVVM/ViewModel/LauncherViewModel.cs
@@ -4,5 +4,10 @@ namespace Launcher.MVVM.ViewModel;
 
 public partial class LauncherViewModel : ObservableObject
 {
+    [ObservableProperty] private double _progress;
+    [ObservableProperty] private string _progressBarVisibility = "Collapsed";
+    [ObservableProperty] private string? _progressInfo;
+    [ObservableProperty] private bool _isProgressIndeterminate;
+    
     
 }
