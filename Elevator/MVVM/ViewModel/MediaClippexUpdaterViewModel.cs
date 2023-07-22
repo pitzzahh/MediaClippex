865898883e97e6482a41966c0f765966708cc241
pitzzahh <pitzzahh@gmail.com>
2023-07-23 00:15:55 +0800

    chore: add version reading from vile #40

--- Elevator/MVVM/ViewModel/MediaClippexUpdaterViewModel.cs
+++ Elevator/MVVM/ViewModel/MediaClippexUpdaterViewModel.cs
@@ -4,11 +4,11 @@ using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Net.Http;
-using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
 using CommunityToolkit.Mvvm.ComponentModel;
 using Octokit;
+using Russkyc.AttachedUtilities.FileStreamExtensions;
 using Application = System.Windows.Application;
 using FileMode = System.IO.FileMode;
 
@@ -25,65 +25,82 @@ public partial class MediaClippexUpdaterViewModel : ObservableObject
     [ObservableProperty] private string? _latestVersion;
     [ObservableProperty] private string? _changeLog;
     [ObservableProperty] private string _showChangeLog = "Collapsed";
+    private static readonly string VersionFilePath = Path.Combine(AppContext.BaseDirectory, "app.version");
     private static string Owner => "pitzzahh";
     private static string Repo => "MediaClippex";
 
     public MediaClippexUpdaterViewModel()
     {
-        CurrentVersion = ReadCurrentVersion();
-
-        var gitHubClient = new GitHubClient(new ProductHeaderValue(Repo));
-
-        Task.Run(async () =>
+        try
         {
-            ProgressBarVisibility = "Visible";
-            IsProgressIndeterminate = true;
-            ProgressInfo = "Getting Latest Version...";
+            CurrentVersion = ReadCurrentVersion();
 
-            var latestRelease = await gitHubClient
-                .Repository
-                .Release
-                .GetLatest(Owner, Repo);
+            if (CurrentVersion is null) return;
 
-            LatestVersion = latestRelease.TagName;
+            var gitHubClient = new GitHubClient(new ProductHeaderValue(Repo));
 
-            IsProgressIndeterminate = false;
-            ProgressInfo = "Getting Changelog...";
-            IsProgressIndeterminate = true;
-
-            ProgressBarVisibility = "Collapsed";
-
-            ChangeLog = latestRelease.Body.Replace("#", "");
-
-            try
+            Task.Run(async () =>
             {
-                var downloadUrl = latestRelease
-                    .Assets
-                    .Where(asset => asset.ContentType == "application/zip")
-                    .FirstOrDefault(asset => asset.Name == "Update.zip");
-
-                if (downloadUrl == null)
-                {
-                    throw new Exception("Unable to find the download URL for the update.");
-                }
-
-                TitleName = "Downloading Update";
-
                 ProgressBarVisibility = "Visible";
-                IsProgressIndeterminate = false;
-                ProgressInfo = "Downloading Update...";
+                IsProgressIndeterminate = true;
+                ProgressInfo = "Getting Latest Version...";
 
-                await DownloadAndInstallUpdate(downloadUrl);
-            }
-            catch (Exception e)
-            {
-                MessageBox.Show($"Error occurred while downloading the update: {e.Message}", "Update Error");
-                Application.Current.Dispatcher.Invoke(() => { Application.Current.Shutdown(); });
-            }
-        });
+                var latestRelease = await gitHubClient
+                    .Repository
+                    .Release
+                    .GetLatest(Owner, Repo);
+
+                LatestVersion = latestRelease.TagName;
+                if (CurrentVersion is { Length: 0 })
+                {
+                    VersionFilePath.StreamWrite(LatestVersion);
+                }
+                
+                if (!ShouldUpdate(CurrentVersion, LatestVersion)) return;
+
+                IsProgressIndeterminate = false;
+                ProgressInfo = "Getting Changelog...";
+                IsProgressIndeterminate = true;
+
+                ProgressBarVisibility = "Collapsed";
+
+                ChangeLog = latestRelease.Body.Replace("#", "");
+
+                try
+                {
+                    var downloadUrl = latestRelease
+                        .Assets
+                        .Where(asset => asset.ContentType == "application/zip")
+                        .FirstOrDefault(asset => asset.Name == "Update.zip");
+
+                    if (downloadUrl == null)
+                    {
+                        throw new Exception("Unable to find the download URL for the update.");
+                    }
+
+                    TitleName = "Downloading Update";
+
+                    ProgressBarVisibility = "Visible";
+                    IsProgressIndeterminate = false;
+                    ProgressInfo = "Downloading Update...";
+
+                    await DownloadAndInstallUpdate(downloadUrl, LatestVersion);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show($"Error occurred while downloading the update: {e.Message}", "Update Error");
+                    Application.Current.Dispatcher.Invoke(() => { Application.Current.Shutdown(); });
+                }
+            });
+        }
+        catch (Exception e)
+        {
+            MessageBox.Show($"Error occurred: {e.Message}", "Error");
+            Application.Current.Dispatcher.Invoke(() => { Application.Current.Shutdown(); });
+        }
     }
 
-    private async Task DownloadAndInstallUpdate(ReleaseAsset releaseAsset)
+    private async Task DownloadAndInstallUpdate(ReleaseAsset releaseAsset, string newVersion)
     {
         using var httpClient = new HttpClient();
         var tempFilePath = Path.GetTempFileName();
@@ -114,10 +131,10 @@ public partial class MediaClippexUpdaterViewModel : ObservableObject
         }
 
         ProgressInfo = "Installing Update...";
-        await Installation(tempFilePath);
+        await Installation(tempFilePath, newVersion);
     }
 
-    private async Task Installation(string zipFilePath)
+    private async Task Installation(string zipFilePath, string newVersion)
     {
         var extractionPath = AppContext.BaseDirectory;
 
@@ -160,21 +177,26 @@ public partial class MediaClippexUpdaterViewModel : ObservableObject
             }
 
             if (File.Exists(zipFilePath)) File.Delete(zipFilePath);
-
+            VersionFilePath.StreamWrite(newVersion);
             var result = MessageBox.Show("Update installed successfully. Do you want to open the application now?",
                 "Update Installed", MessageBoxButton.YesNo);
             if (result == MessageBoxResult.Yes)
             {
-                Process.Start("MediaClippex.exe");
+                Process.Start(Path.Combine(AppContext.BaseDirectory, "MediaClippex.exe"));
             }
 
             Application.Current.Dispatcher.Invoke(() => { Application.Current.Shutdown(); });
         });
     }
-    
+
     private static string? ReadCurrentVersion()
     {
-        return Assembly.GetCallingAssembly().GetName().Version?.ToString();
+        return VersionFilePath.StreamRead();
+    }
+    
+    private static bool ShouldUpdate(string currentVersion, string latestVersion)
+    {
+        return new Version(latestVersion) > new Version(currentVersion);
     }
 
     private void OnDownloadProgress(long bytesReceived, long totalBytes)
