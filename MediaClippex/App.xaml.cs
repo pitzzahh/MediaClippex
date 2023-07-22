a01ec40eaf2078a76f59da334b31e59241a53035
pitzzahh <pitzzahh@gmail.com>
2023-07-23 00:14:40 +0800

    chore: add creating version on file #40

--- MediaClippex/App.xaml.cs
+++ MediaClippex/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using MediaClippex.Services;
+﻿using System;
+using System.IO;
+using MediaClippex.MVVM.ViewModel;
+using MediaClippex.Services;
+using Russkyc.AttachedUtilities.FileStreamExtensions;
 using Russkyc.DependencyInjection.Implementations;
 
 namespace MediaClippex;
@@ -11,5 +15,10 @@ public partial class App
     public App()
     {
         BuilderServices.BuildWithContainer(BuildContainer.ConfigureServices());
+        var versionFilePath = Path.Combine(AppContext.BaseDirectory, "app.version");
+        if (!File.Exists(versionFilePath))
+        {
+            versionFilePath.StreamWrite(CheckUpdateViewModel.ReadCurrentVersion());
+        }
     }
 }
