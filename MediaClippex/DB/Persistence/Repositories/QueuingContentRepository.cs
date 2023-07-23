1fec25094e7ef8d334d18443b2c8b5d9998693fb
pitzzahh <pitzzahh@gmail.com>
2023-07-23 23:19:16 +0800

    chore: add QueuingContentRepository for saving paused downloads

---
+++ MediaClippex/DB/Persistence/Repositories/QueuingContentRepository.cs
@@ -0,0 +1,13 @@
+﻿using MediaClippex.MVVM.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediaClippex.DB.Persistence.Repositories;
+
+public class QueuingContentRepository : Repository<QueuingVideo>
+{
+
+    public QueuingContentRepository(DbContext context) : base(context)
+    {
+    }
+
+}
