fedea4cfad369b4415ff5030ef840f5c674c2970
pitzzahh <pitzzahh@gmail.com>
2023-08-01 17:19:15 +0800

    chore: add FileType field

--- MediaClippex/MVVM/Model/QueuingVideo.cs
+++ MediaClippex/MVVM/Model/QueuingVideo.cs
@@ -6,10 +6,11 @@ public class QueuingVideo
 {
 
     [Key]
-    public int QueuingVideoId;
+    public int QueuingVideoId { get; set; }
     public string Title { get; set; }
     public string Duration { get; set; }
     public string ThumbnailUrl { get; set; }
+    public string FileType { get; set; }
     public string Url { get; set; }
     public double Progress { get; set; }
     public string ProgressInfo { get; set; }
@@ -17,13 +18,14 @@ public class QueuingVideo
     public bool Paused { get; set; }
     public bool IsAudioOnly { get; set; }
 
-    public QueuingVideo(string title, string duration, string thumbnailUrl, string url, double progress,
+    public QueuingVideo(string title, string duration, string thumbnailUrl, string fileType, string url, double progress,
         string progressInfo, string selectedQuality, bool paused, bool isAudioOnly = false)
 
     {
         Title = title;
         Duration = duration;
         ThumbnailUrl = thumbnailUrl;
+        FileType = fileType;
         Url = url;
         Progress = progress;
         ProgressInfo = progressInfo;
