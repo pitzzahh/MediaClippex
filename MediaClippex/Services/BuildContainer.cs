ad4b6f61373046f0aa7bff90f380cfc3bb0219f6
pitzzahh <pitzzahh@gmail.com>
2023-08-20 23:55:07 +0800

    chore: added more services

--- MediaClippex/Services/BuildContainer.cs
+++ MediaClippex/Services/BuildContainer.cs
@@ -1,4 +1,5 @@
-﻿using MediaClippex.MVVM.ViewModel;
+﻿using MediaClippex.MVVM.View;
+using MediaClippex.MVVM.ViewModel;
 using Russkyc.DependencyInjection.Implementations;
 using Russkyc.DependencyInjection.Interfaces;
 
@@ -10,8 +11,12 @@ public static class BuildContainer
     // Returns container with registered dependencies
     public static IServicesContainer ConfigureServices()
     {
-        return new ServicesCollection()
+        return new ServicesContainer()
+            .AddSingleton<StorageService>()
             .AddSingleton<VideoInfoCardViewModel>()
+            .AddSingleton<MediaClippexViewModel>()
+            .AddTransient<CheckUpdateViewModel>()
+            .AddSingleton<CheckUpdateView>()
             .Build();
     }
 }
