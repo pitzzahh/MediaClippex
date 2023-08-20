584e240aa5ce74b08c6d3e7c7fd9c07ed0ef1215
pitzzahh <pitzzahh@gmail.com>
2023-08-21 00:51:44 +0800

    chore: add storage service

---
+++ MediaClippex/Services/StorageService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using MediaClippex.MVVM.ViewModel;
+using Russkyc.DependencyInjection.Implementations;
+
+namespace MediaClippex.Services;
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public class StorageService
+{
+    public readonly ObservableCollection<DownloadedVideoCardViewModel> DownloadedVideoCardViewModelsList = new();
+
+    public readonly ObservableCollection<QueuingContentCardViewModel> QueuingContentCardViewModelsList = new();
+
+    public void RemoveFromQueue(string title)
+    {
+        var queuingContentCardViewModel = BuilderServices.Resolve<StorageService>()
+            .QueuingContentCardViewModelsList
+            .FirstOrDefault(s => s.Title.Equals(title));
+        if (queuingContentCardViewModel != null)
+            QueuingContentCardViewModelsList
+                .Remove(
+                    queuingContentCardViewModel
+                );
+    }
+}
