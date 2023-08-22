7932213adf0ee78adcf15e510823a6d85ca012cf
pitzzahh <pitzzahh@gmail.com>
2023-08-22 13:36:23 +0800

    chore: added/refactor helper methods

--- MediaClippex/Services/StorageService.cs
+++ MediaClippex/Services/StorageService.cs
@@ -1,5 +1,6 @@
-﻿using System.Collections.ObjectModel;
-using System.Linq;
+﻿using System.Linq;
+using System.Windows;
+using MediaClippex.DB.Core;
 using MediaClippex.MVVM.ViewModel;
 using Russkyc.DependencyInjection.Implementations;
 
@@ -8,19 +9,45 @@ namespace MediaClippex.Services;
 // ReSharper disable once ClassNeverInstantiated.Global
 public class StorageService
 {
-    public readonly ObservableCollection<DownloadedVideoCardViewModel> DownloadedVideoCardViewModelsList = new();
+    private readonly IUnitOfWork _unitOfWork;
 
-    public readonly ObservableCollection<QueuingContentCardViewModel> QueuingContentCardViewModelsList = new();
+    public StorageService(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
 
     public void RemoveFromQueue(string title)
     {
-        var queuingContentCardViewModel = BuilderServices.Resolve<StorageService>()
-            .QueuingContentCardViewModelsList
-            .FirstOrDefault(s => s.Title.Equals(title));
-        if (queuingContentCardViewModel != null)
-            QueuingContentCardViewModelsList
-                .Remove(
-                    queuingContentCardViewModel
-                );
+        var mediaClippexViewModel = BuilderServices.Resolve<MediaClippexViewModel>();
+        var queuingContentCardViewModels = mediaClippexViewModel.QueuingContentCardViewModels;
+        queuingContentCardViewModels
+            .Remove(
+                mediaClippexViewModel
+                    .QueuingContentCardViewModels
+                    .First(s => s.Title.Equals(title))
+            );
+
+        var foundQueuingVideo = _unitOfWork.QueuingContentRepository
+            .Find(v => v.Title.Equals(title))
+            .FirstOrDefault();
+        MessageBox.Show($"Found Queuing Video {foundQueuingVideo}");
+
+        if (foundQueuingVideo == null) return;
+        _unitOfWork.QueuingContentRepository
+            .Remove(foundQueuingVideo);
+
+        if (_unitOfWork.Complete() == 0) return;
+        MessageBox.Show("Queuing Video Deleted From DB");
+        CheckQueue(mediaClippexViewModel);
+    }
+
+    public static void CheckQueue(MediaClippexViewModel mediaClippexViewModel = default!)
+    {
+        mediaClippexViewModel.HasQueue = mediaClippexViewModel.QueuingContentCardViewModels.Count > 0;
+    }
+
+    public static void CheckDownloadHistory(MediaClippexViewModel mediaClippexViewModel = default!)
+    {
+        mediaClippexViewModel.HasDownloadHistory = mediaClippexViewModel.DownloadedVideoCardViewModels.Count > 0;
     }
 }
