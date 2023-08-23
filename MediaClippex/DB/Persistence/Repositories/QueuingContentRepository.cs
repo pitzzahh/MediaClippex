a40bdf9cacf92562fc20fe555e363342c30f9f32
pitzzahh <pitzzahh@gmail.com>
2023-08-23 10:41:57 +0800

    chore: rename QueuingVideo to QueuingContent

--- MediaClippex/DB/Persistence/Repositories/QueuingContentRepository.cs
+++ MediaClippex/DB/Persistence/Repositories/QueuingContentRepository.cs
@@ -3,11 +3,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace MediaClippex.DB.Persistence.Repositories;
 
-public class QueuingContentRepository : Repository<QueuingVideo>
+public class QueuingContentRepository : Repository<QueuingContent>
 {
-
     public QueuingContentRepository(DbContext context) : base(context)
     {
     }
-
 }
