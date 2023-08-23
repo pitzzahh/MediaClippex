417c563b2d3ed175ed1f198483738c92635baa9d
pitzzahh <pitzzahh@gmail.com>
2023-08-23 10:42:46 +0800

    chore: removed extra spaces

--- MediaClippex/MVVM/ViewModel/VideoInfoCardViewModel.cs
+++ MediaClippex/MVVM/ViewModel/VideoInfoCardViewModel.cs
@@ -6,10 +6,7 @@ namespace MediaClippex.MVVM.ViewModel;
 public partial class VideoInfoCardViewModel : BaseViewModel
 {
     [ObservableProperty] private string? _description;
-
     [ObservableProperty] private string? _duration;
-
     [ObservableProperty] private string? _imageUrl;
-
     [ObservableProperty] private string? _title;
 }
