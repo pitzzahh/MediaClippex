216b5fd3f91dd44694808a1774d6637d89cc88fa
pitzzahh <pitzzahh@gmail.com>
2023-08-23 23:58:59 +0800

    chore: removed filesize field

--- MediaClippex/MVVM/Model/Video.cs
+++ MediaClippex/MVVM/Model/Video.cs
@@ -4,25 +4,23 @@ namespace MediaClippex.MVVM.Model;
 
 public class Video
 {
-    [Key]
-    public int VideoId { get; set; }
-    
-    public string ThumbnailUrl { get; set; }
-    public string? Title { get; set; }
-    public string? Duration { get; set; }
-    public string? Description { get; set; }
-    public string? FileType { get; set; }
-    public string? FileSize { get; set; }
-    public string? Path { get; set; }
-
-    public Video(string thumbnailUrl, string? title, string? duration, string? description, string? fileType, string? fileSize, string? path)
+    public Video(string thumbnailUrl, string? title, string? duration, string? description, string? fileType,
+        string? path)
     {
         ThumbnailUrl = thumbnailUrl;
         Title = title;
         Duration = duration;
         Description = description;
         FileType = fileType;
-        FileSize = fileSize;
         Path = path;
     }
+
+    [Key] public int VideoId { get; set; }
+
+    public string ThumbnailUrl { get; set; }
+    public string? Title { get; set; }
+    public string? Duration { get; set; }
+    public string? Description { get; set; }
+    public string? FileType { get; set; }
+    public string? Path { get; set; }
 }
