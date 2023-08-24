5667ab7b40aa4b1646cd054d103e893a32326817
pitzzahh <pitzzahh@gmail.com>
2023-08-24 11:23:35 +0800

    chore: fixed playing of downloaded content

--- MediaClippex/MVVM/ViewModel/DownloadedVideoCardViewModel.cs
+++ MediaClippex/MVVM/ViewModel/DownloadedVideoCardViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.IO;
+using System.Windows;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
@@ -8,20 +10,20 @@ namespace MediaClippex.MVVM.ViewModel;
 // ReSharper disable once ClassNeverInstantiated.Global
 public partial class DownloadedVideoCardViewModel : BaseViewModel
 {
-    [ObservableProperty] private string? _title;
-
     [ObservableProperty] private string? _description;
-    
-    [ObservableProperty] private string? _fileType;
-    [ObservableProperty] private string? _fileSize;
-    
-    [ObservableProperty] private string? _path;
 
     [ObservableProperty] private string? _duration;
+    [ObservableProperty] private string? _fileSize;
+
+    [ObservableProperty] private string? _fileType;
 
     [ObservableProperty] private string? _imageUrl;
 
-    public DownloadedVideoCardViewModel(string? title, string? description, string? fileType, string? fileSize, string? path, string? duration, string? imageUrl)
+    [ObservableProperty] private string? _path;
+    [ObservableProperty] private string? _title;
+
+    public DownloadedVideoCardViewModel(string? title, string? description, string? fileType, string? fileSize,
+        string? path, string? duration, string? imageUrl)
     {
         Title = title;
         Description = description;
@@ -36,7 +38,18 @@ public partial class DownloadedVideoCardViewModel : BaseViewModel
     private void OpenVideo()
     {
         if (string.IsNullOrEmpty(Path) || !File.Exists(Path)) return;
-        Process.Start("explorer.exe", $"/select,\"{Path}\"");
+        try
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = Path,
+                UseShellExecute = true
+            });
+        }
+        catch (Exception ex)
+        {
+            // Handle any exceptions that might occur
+            MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
     }
-
 }
