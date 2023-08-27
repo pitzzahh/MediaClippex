324a9de43d71ea42cb00db38667de2a2d3600c1c
pitzzahh <pitzzahh@gmail.com>
2023-08-27 20:17:26 +0800

    chore: added delete video function

--- MediaClippex/MVVM/ViewModel/DownloadedVideoCardViewModel.cs
+++ MediaClippex/MVVM/ViewModel/DownloadedVideoCardViewModel.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using MediaClippex.DB.Core;
+using Russkyc.DependencyInjection.Implementations;
 
 namespace MediaClippex.MVVM.ViewModel;
 
@@ -32,7 +36,10 @@ public partial class DownloadedVideoCardViewModel : BaseViewModel
         Path = path;
         Duration = duration;
         ImageUrl = imageUrl;
+        UnitOfWork = BuilderServices.Resolve<IUnitOfWork>();
     }
+
+    private IUnitOfWork UnitOfWork { get; }
 
     [RelayCommand]
     private void OpenVideo()
@@ -50,6 +57,30 @@ public partial class DownloadedVideoCardViewModel : BaseViewModel
         {
             // Handle any exceptions that might occur
             MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+
+    [RelayCommand]
+    private async Task DeleteVideo()
+    {
+        if (string.IsNullOrEmpty(Path) || !File.Exists(Path)) return;
+        try
+        {
+            var foundDownloadedVideo = UnitOfWork.VideosRepository
+                .Find(v => v.Title != null && v.Title.Equals(Title))
+                .FirstOrDefault();
+
+            if (foundDownloadedVideo == null) return;
+
+            UnitOfWork.VideosRepository
+                .Remove(foundDownloadedVideo);
+            if (UnitOfWork.Complete() == 0) return;
+            File.Delete(Path);
+            await Task.Run(BuilderServices.Resolve<MediaClippexViewModel>().GetDownloadedVideos);
+        }
+        catch (Exception)
+        {
+            // Handle any exceptions that might occur
         }
     }
 }
