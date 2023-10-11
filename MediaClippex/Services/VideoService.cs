88682957309d45f2475dfe23ebae87a652e00bf2
pitzzahh <pitzzahh@gmail.com>
2023-10-11 21:47:59 +0800

    chore: added getting playlist info

--- MediaClippex/Services/VideoService.cs
+++ MediaClippex/Services/VideoService.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using YoutubeExplode;
+using YoutubeExplode.Common;
 using YoutubeExplode.Converter;
+using YoutubeExplode.Playlists;
 using YoutubeExplode.Videos;
 using YoutubeExplode.Videos.Streams;
 
@@ -18,9 +21,20 @@ public static class VideoService
         return await Youtube.Videos.GetAsync(url);
     }
 
-    public static async Task<StreamManifest> GetManifest(string url)
+
+    public static async Task<Playlist> GetPlaylists(string url)
+    {
+        return await Youtube.Playlists.GetAsync(url);
+    }
+
+    public static async Task<StreamManifest> GetVideoManifest(string url)
     {
         return await Youtube.Videos.Streams.GetManifestAsync(url);
+    }
+
+    public static async Task<IReadOnlyList<PlaylistVideo>> GetPlaylistVideos(string playlistUrl)
+    {
+        return await Youtube.Playlists.GetVideosAsync(playlistUrl);
     }
 
     public static IVideoStreamInfo GetVideoOnlyStreamInfo(StreamManifest manifest, string quality)
@@ -61,7 +75,8 @@ public static class VideoService
     {
         if (!video.Duration.HasValue) return string.Empty;
         var duration = video.Duration.Value.TotalSeconds;
-        var fileSize = (long)(GetAudioOnlyStream(manifest, selectedQuality).Bitrate.BitsPerSecond +  GetVideoOnlyStreamInfo(manifest, selectedQuality).Bitrate.BitsPerSecond * duration / 8);
+        var fileSize = (long)(GetAudioOnlyStream(manifest, selectedQuality).Bitrate.BitsPerSecond +
+                              GetVideoOnlyStreamInfo(manifest, selectedQuality).Bitrate.BitsPerSecond * duration / 8);
         return StringService.ConvertBytesToFormattedString(fileSize);
     }
 
