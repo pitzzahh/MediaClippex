599d37e81f4179196eea1e599e9cf6f5e4b52fe8
pitzzahh <pitzzahh@gmail.com>
2023-10-21 21:01:00 +0800

    chore: add setting vm

--- MediaClippex/MVVM/ViewModel/SettingsViewModel.cs
+++ MediaClippex/MVVM/ViewModel/SettingsViewModel.cs
@@ -1,6 +1,64 @@
-﻿namespace MediaClippex.MVVM.ViewModel;
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Material.Icons;
+using Material.Icons.WPF;
+using MediaClippex.Services.Settings.Interfaces;
+using Microsoft.Win32;
+using org.russkyc.moderncontrols.Helpers;
+using Russkyc.DependencyInjection.Attributes;
+using Russkyc.DependencyInjection.Enums;
 
-public class SettingsViewModel
+namespace MediaClippex.MVVM.ViewModel;
+
+[Service(Scope.Singleton)]
+// ReSharper disable once ClassNeverInstantiated.Global
+public partial class SettingsViewModel : BaseViewModel
 {
-    
+    [ObservableProperty] private string? _color;
+    [ObservableProperty] private bool _isNightMode;
+    private bool _nightMode = true;
+    [ObservableProperty] private MaterialIcon? _themeIcon;
+    [ObservableProperty] private ObservableCollection<string> _themes = new();
+
+    public SettingsViewModel(ISettings settings)
+    {
+        ThemeManager.Instance
+            .GetColorThemes()
+            .ToList()
+            .ForEach(Themes.Add);
+        NightMode = settings.IsDarkMode();
+
+        SystemEvents.UserPreferenceChanged += (_, e) =>
+        {
+            if (e.Category != UserPreferenceCategory.General) return;
+            NightMode = settings.IsDarkMode();
+        };
+    }
+
+    // ReSharper disable once MemberCanBePrivate.Global
+    public bool NightMode
+    {
+        get => _nightMode;
+        // ReSharper disable once PropertyCanBeMadeInitOnly.Global
+        // ReSharper disable once UnusedMember.Global
+        set
+        {
+            _nightMode = value;
+            var materialIcon = new MaterialIcon
+            {
+                Kind = value ? MaterialIconKind.WeatherSunny : MaterialIconKind.MoonWaxingCrescent
+            };
+            ThemeIcon = materialIcon;
+            OnPropertyChanged();
+            ThemeManager.Instance.SetBaseTheme(NightMode ? "Dark" : "Light");
+        }
+    }
+
+    [RelayCommand]
+    private static void ChangeColor(string color)
+    {
+        ThemeManager.Instance.SetColorTheme(color);
+    }
 }
