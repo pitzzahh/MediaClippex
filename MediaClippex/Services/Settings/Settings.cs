30f06b17e01093bf30051e09dc17c4783b6e9f5a
pitzzahh <pitzzahh@gmail.com>
2023-10-21 19:25:14 +0800

    chore: add Settings.cs

---
+++ MediaClippex/Services/Settings/Settings.cs
@@ -0,0 +1,25 @@
+﻿using MediaClippex.Services.Settings.Interfaces;
+using Microsoft.Win32;
+using Russkyc.DependencyInjection.Attributes;
+using Russkyc.DependencyInjection.Enums;
+
+namespace MediaClippex.Services.Settings;
+
+[Service(Scope.Singleton, Registration.AsInterfaces)]
+public class Settings : ISettings
+{
+    public bool IsDarkMode()
+    {
+        try
+        {
+            return Registry.CurrentUser.OpenSubKey(
+                @"SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize",
+                false
+            )?.GetValue("AppsUseLightTheme") is 0;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
