7efa3bcd1d0a08e9d5323c8a08c8486a08bcdcb8
pitzzahh <pitzzahh@gmail.com>
2023-10-21 20:59:45 +0800

    chore: temporary add settings navigation

--- MediaClippex/MVVM/ViewModel/MainViewModel.cs
+++ MediaClippex/MVVM/ViewModel/MainViewModel.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using MediaClippex.Services;
 using org.russkyc.moderncontrols.Helpers;
 using Russkyc.DependencyInjection.Attributes;
 using Russkyc.DependencyInjection.Enums;
@@ -15,10 +14,7 @@ public partial class MainViewModel : BaseViewModel
 {
     private readonly IServicesContainer _container;
     [ObservableProperty] private BaseViewModel? _context;
-    [ObservableProperty] private bool _isHome;
-    [ObservableProperty] private bool _isNightMode;
     private int _selectedIndex;
-    private bool _nightMode = true;
     [ObservableProperty] private ObservableCollection<string> _themes = new();
 
     public MainViewModel(IServicesContainer container)
@@ -29,25 +25,9 @@ public partial class MainViewModel : BaseViewModel
             .ToList()
             .ForEach(Themes.Add);
         SelectedIndex = 3;
-        IsHome = true;
         Context = _container.Resolve<HomeViewModel>();
-        NightMode = SettingsService.IsDarkModeEnabledByDefault();
     }
 
-    // ReSharper disable once MemberCanBePrivate.Global
-    public bool NightMode
-    {
-        get => _nightMode;
-        // ReSharper disable once PropertyCanBeMadeInitOnly.Global
-        // ReSharper disable once UnusedMember.Global
-        set
-        {
-            _nightMode = value;
-            OnPropertyChanged();
-            ThemeManager.Instance.SetBaseTheme(NightMode ? "Dark" : "Light");
-        }
-    }
-    
     // ReSharper disable once MemberCanBePrivate.Global
     public int SelectedIndex
     {
@@ -62,30 +42,27 @@ public partial class MainViewModel : BaseViewModel
     }
 
     [RelayCommand]
-    private void NavigateToHome() // violates dry, will fix soon
+    private void NavigateToHome() // violates dry principle, will fix soon
     {
-        var homeViewModel = _container.Resolve<HomeViewModel>();
-        if (Context == homeViewModel) return;
-
-        IsHome = true;
-        Context = homeViewModel;
+        var viewModel = _container.Resolve<HomeViewModel>();
+        if (Context == viewModel) return;
+        Context = viewModel;
     }
 
     [RelayCommand]
-    private void NavigateToAbout() // violates dry, will fix soon (also violates dry, the comment)
+    private void NavigateToAbout() // violates dry principle, will fix soon (also violates dry principle, the comment)
     {
-        var aboutViewModel = _container.Resolve<AboutViewModel>();
-        if (Context == aboutViewModel) return;
-        IsHome = false;
-        Context = aboutViewModel;
+        var viewModel = _container.Resolve<AboutViewModel>();
+        if (Context == viewModel) return;
+        Context = viewModel;
     }
-    
+
     [RelayCommand]
-    private void NavigateToSettings() // violates dry, will fix soon (also violates dry, the comment)
+    private void
+        NavigateToSettings() // violates dry principle, will fix soon (also violates dry principle, the comment)
     {
-        var aboutViewModel = _container.Resolve<AboutViewModel>();
-        if (Context == aboutViewModel) return;
-        IsHome = false;
-        Context = aboutViewModel;
+        var viewModel = _container.Resolve<SettingsViewModel>();
+        if (Context == viewModel) return;
+        Context = viewModel;
     }
 }
