10960045b386f07b5483c7c2baea3e60ad98a9d1
pitzzahh <pitzzahh@gmail.com>
2023-10-27 08:50:44 +0800

    chore: removed color theme component

--- MediaClippex/MVVM/ViewModel/MainViewModel.cs
+++ MediaClippex/MVVM/ViewModel/MainViewModel.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections.ObjectModel;
-using System.Linq;
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using org.russkyc.moderncontrols.Helpers;
 using Russkyc.DependencyInjection.Attributes;
 using Russkyc.DependencyInjection.Enums;
 using Russkyc.DependencyInjection.Interfaces;
@@ -14,31 +11,11 @@ public partial class MainViewModel : BaseViewModel
 {
     private readonly IServicesContainer _container;
     [ObservableProperty] private BaseViewModel? _context;
-    private int _selectedIndex;
-    [ObservableProperty] private ObservableCollection<string> _themes = new();
 
     public MainViewModel(IServicesContainer container)
     {
         _container = container;
-        ThemeManager.Instance
-            .GetColorThemes()
-            .ToList()
-            .ForEach(Themes.Add);
-        SelectedIndex = 3;
         Context = _container.Resolve<HomeViewModel>();
-    }
-
-    // ReSharper disable once MemberCanBePrivate.Global
-    public int SelectedIndex
-    {
-        get => _selectedIndex;
-        // ReSharper disable once PropertyCanBeMadeInitOnly.Global
-        // ReSharper disable once UnusedMember.Global
-        set
-        {
-            _selectedIndex = value;
-            ThemeManager.Instance.SetColorTheme(Themes[SelectedIndex]);
-        }
     }
 
     [RelayCommand]
