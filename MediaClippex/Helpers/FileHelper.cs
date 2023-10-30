9c8f438a0f9678c84e02ee97e40a38cfe5e1f191
pitzzahh <pitzzahh@gmail.com>
2023-10-30 22:11:55 +0800

    :technologist: add FileHelper.cs when deleting files

---
+++ MediaClippex/Helpers/FileHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace MediaClippex.Helpers;
+
+public static class FileHelper
+{
+    public static void Delete(string? path, bool checkIfExists = true)
+    {
+        if (checkIfExists && path != null && File.Exists(path)) File.Delete(path);
+    }
+
+    public static void DeleteBatch(IEnumerable<string> paths, bool checkIfExists = true)
+    {
+        foreach (var path in paths) Delete(path, checkIfExists);
+    }
+}
