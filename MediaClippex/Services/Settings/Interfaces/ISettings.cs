e644296141b223c6d8062429eb3c8f037f6ee341
pitzzahh <pitzzahh@gmail.com>
2023-11-12 19:14:00 +0800

    :wrench: Add color theme and download path

--- MediaClippex/Services/Settings/Interfaces/ISettings.cs
+++ MediaClippex/Services/Settings/Interfaces/ISettings.cs
@@ -3,5 +3,7 @@
 public interface ISettings
 {
     bool IsDarkMode();
+    string ColorTheme(bool change = false, string colorTheme = "");
     void ListenToThemeChange(bool data = false);
+    string DownloadPath(bool change = false, string downloadPath = "");
 }
