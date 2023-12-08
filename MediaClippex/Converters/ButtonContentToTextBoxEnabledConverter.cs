44e859f3478faf8691d7394462618fc3ef8ec67a
pitzzahh <pitzzahh@gmail.com>
2023-12-08 16:42:16 +0800

    :wrench: Add converter

---
+++ MediaClippex/Converters/ButtonContentToTextBoxEnabledConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MediaClippex.Converters;
+
+public class ButtonContentToTextBoxEnabledConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value?.ToString() == "Save";
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return (bool)value! ? "Save" : "Change";
+    }
+}
