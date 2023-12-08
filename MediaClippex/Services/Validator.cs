2fdd94764317bdced884d03832160c1f8a925ff0
pitzzahh <pitzzahh@gmail.com>
2023-12-08 16:43:54 +0800

    :safety_vest: Add Validator.cs validating string if number or not

---
+++ MediaClippex/Services/Validator.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace MediaClippex.Services;
+
+public static class Validator
+{
+    /// <summary>
+    ///     Checks if a given string is a number.
+    /// </summary>
+    /// <param name="input">The string to be validated.</param>
+    /// <param name="wholeNumber">Specify whether the number should be a whole number or not.</param>
+    /// <returns>True if the string is a valid number based on the criteria, false otherwise.</returns>
+    public static bool IsNumber(string input, bool wholeNumber = false)
+    {
+        return Regex.IsMatch(input, wholeNumber ? @"^\d+$" : @"^-?\d+(\.\d+)?$");
+    }
+}
